768a36e7bce1547046412b6d2ade07006e6be081
Max Jacobson <max@hardscrabble.net>
2014-05-18 21:02:19 -0400

    Add a home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#about'
+  root to: 'pages#home'
   get '/about' => 'pages#about'
   get '/auth/instapaper/callback' => 'sessions#create'
   delete '/aurevoir' => 'sessions#destroy', as: 'logout'
