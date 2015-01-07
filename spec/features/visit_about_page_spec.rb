a909007afc0b8e8acf8ef97b5ddff6edae9ebb93
Max Jacobson <max@hardscrabble.net>
2015-01-06 19:14:15 -0500

    Fix a failing spec

--- spec/features/visit_about_page_spec.rb
+++ spec/features/visit_about_page_spec.rb
@@ -4,7 +4,7 @@ feature 'User wants to learn more about Layabout' do
   scenario 'User visits about page' do
     visit about_path
     page.should have_text 'About'
-    page.should have_text 'film_snob'
+    page.should have_text 'film snob'
   end
 end
 
